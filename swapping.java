eeb0b556f47f09f1d41d0b0c9d05cac53a0f77f1
4malleswari <122262828+4malleswari@users.noreply.github.com>
2024-06-29 08:57:56 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class swapping
+{
+      public static void main(String arg[])
+      {
+      Scanner sc=new Scanner(System.in);
+      int a,b,temp;
+      System.out.println("enter the a value");
+      a=sc.nextInt();
+      System.out.println("enter the b value");
+      b=sc.nextInt();
+      temp=a;
+      a=b;
+      b=temp;
+      System.out.println("After swapping:"+a);
+      System.out.println("After swapping:"+b);
+      }
+}
+
+
+
+                      
